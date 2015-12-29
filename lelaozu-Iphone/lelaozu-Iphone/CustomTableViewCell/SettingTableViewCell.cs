55f09ac8e73295b699b03d167e896b4408220302
硕阳 <shuoyang@shuoyangdeMacBook-Pro.local>
2015-12-29 15:31:35 +0800

    m

---
+++ lelaozu-Iphone/lelaozu-Iphone/CustomTableViewCell/SettingTableViewCell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace lelaozuIphone
+{
+	public partial class SettingTableViewCell : UITableViewCell
+	{
+		public static readonly NSString Key = new NSString ("SettingTableViewCell");
+		public static readonly UINib Nib;
+
+		static SettingTableViewCell ()
+		{
+			Nib = UINib.FromName ("SettingTableViewCell", NSBundle.MainBundle);
+		}
+
+		public SettingTableViewCell (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
