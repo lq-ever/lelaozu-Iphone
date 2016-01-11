a59d019f69b24a5c97fd8a569851dc7ed7b973fc
Jonny <lq.linknet@gmil.com>
2016-01-11 13:34:28 +0800

    ok

---
+++ lelaozu-Iphone/lelaozu-Iphone/obj/Xcode/0/AlarmListTableViewCell.h
@@ -0,0 +1,27 @@
+// WARNING
+// This file has been generated automatically by Xamarin Studio to
+// mirror C# types. Changes in this file made by drag-connecting
+// from the UI designer will be synchronized back to C#, but
+// more complex manual changes may not transfer correctly.
+
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+@interface AlarmListTableViewCell : UITableViewCell {
+	UILabel *_lbl_alarmContent;
+	UILabel *_lbl_alarmStatus;
+	UILabel *_lbl_alarmTime;
+	UILabel *_lbl_alarmTrueName;
+}
+
+@property (nonatomic, retain) IBOutlet UILabel *lbl_alarmContent;
+
+@property (nonatomic, retain) IBOutlet UILabel *lbl_alarmStatus;
+
+@property (nonatomic, retain) IBOutlet UILabel *lbl_alarmTime;
+
+@property (nonatomic, retain) IBOutlet UILabel *lbl_alarmTrueName;
+
+@end
---
+++ lelaozu-Iphone/lelaozu-Iphone/obj/Xcode/0/BindGuardianViewController.m
@@ -0,0 +1,16 @@
+// WARNING
+// This file has been generated automatically by Xamarin Studio to
+// mirror C# types. Changes in this file made by drag-connecting
+// from the UI designer will be synchronized back to C#, but
+// more complex manual changes may not transfer correctly.
+
+
+#import "BindGuardianViewController.h"
+
+@implementation BindGuardianViewController
+
+@synthesize btn_Search = _btn_Search;
+@synthesize tableView = _tableView;
+@synthesize txt_Condition = _txt_Condition;
+
+@end
